10cd76cff447347ef55dcaef779f5bef396497a7
SamuelBriggs <tolulope.alawode@gmail.com>
2024-03-27 12:41:47 +0100

    new stuff

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import LoginRedirect from "./pages/LoginRedirect";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />}></Route>
+            <Route path="/" element={<LandingPage />}></Route>
             <Route path="/loginRedirect" element={<LoginRedirect />}></Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,8 @@
+function LandingPage(){
+
+    return(
+        <h1>Welcome here </h1>
+    )
+
+}
+export default LandingPage;
